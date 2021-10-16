a2a6d18f492424f8bc41683db6ec160390d9e80c
kim <manmen.mi1375@gmail.com>
2021-10-16 22:14:56 +0900

    나머지 추가

--- src/component/Kansen.vue
+++ src/component/Kansen.vue
@@ -21,8 +21,20 @@ defineProps({
 
   // polaris
   &akagi_mu { background: url(../icons/polaris.png) -80px 0; }
+  &kaga { background: url(../icons/polaris.png) -120px -40px; }
   &hipper_mu { background: url(../icons/polaris.png) -40px 0; }
-
+  &gascogne_mu { background: url(../icons/polaris.png) -80px -40px; }
+  &sheffield_mu { background: url(../icons/polaris.png) 0 -80px; }
+  &cleveland_mu { background: url(../icons/polaris.png) -160px 0; }
+  &columbia { background: url(../icons/polaris.png) -200px 0; }
+  &montpelier { background: url(../icons/polaris.png) -160px -40px; }
+  &denver { background: url(../icons/polaris.png) 0 -40px; }
+  &eugen { background: url(../icons/polaris.png) -200px -40px; }
+  &spee { background: url(../icons/polaris.png) 0 0; }
+  &deutschland { background: url(../icons/polaris.png) -40px -40px; }
+  &belfast { background: url(../icons/polaris.png) -120px 0; }
+  &shoukaku { background: url(../icons/polaris.png) -40px -80px; }
+  &zuikaku { background: url(../icons/polaris.png) -80px -80px; }
 
 }
 </style>
--- src/component/PostArticles.vue
+++ src/component/PostArticles.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="form-control gap-2 px-4 overflow-y-auto text-sm">
     <div class="flex" :class="{'mb-4': altmode}">
-      <div v-if="altmode" class="avatar p-3 pt-0">
-        <div class="rounded-full w-12 h-12 border-[#888] border-solid border-[0.5px]">
-          <img :src="`/assets/kansen/${icon}.png`">
-        </div>
-      </div>
+      <kansen v-if="altmode" :name="icon"></kansen>
       <p class="whitespace-pre-line w-full" :class="{'py-2': altmode}">
         <span class="font-bold mr-2">{{ kansen }}</span>
         {{ article[lang] }}
@@ -15,11 +11,7 @@
     <router-link :to="{name: 'post', params: {pid}}"><span v-if="!altmode" class="text-gray-400">show all comments...</span></router-link>
 
     <div v-for="c in filteredComments" class="flex" :class="{'ml-12': c.depth}">
-      <div v-if="altmode" class="avatar p-3 pt-0">
-        <div class="rounded-full w-12 h-12 border-[#888] border-solid border-[0.5px]">
-          <img :src="`/assets/kansen/${c.icon}.png`">
-        </div>
-      </div>
+      <kansen v-if="altmode" :name="c.icon"></kansen>
       <p class="whitespace-pre-line w-full" :class="{'py-2': altmode}">
         <span class="font-bold mr-2">{{ c.kansen }}</span>
         {{ c.comment[lang] }}
@@ -31,6 +23,7 @@
 <script lang="ts" setup>
 import { computed } from "vue";
 
+import Kansen from "./Kansen.vue";
 import useLang from "../lang";
 
 const props = defineProps([
