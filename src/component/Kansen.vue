c93864ac0756ea52275491e1f49d0b4ce2f504dd
kim <manmen.mi1375@gmail.com>
2021-10-16 21:41:43 +0900

    아이콘 스프라이트로 한번에 로딩 (일단 샘플로 두개만)

---
+++ src/component/Kansen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="avatar p-3 pt-0">
+    <div class="rounded-full w-[40px] h-[40px] border-[#888] border-solid border-[0.5px]" :class="`icon-${name}`"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+defineProps({
+  name: { type: String, required: true },
+})
+
+</script>
+
+<style lang="scss" scoped>
+.icon- {
+  &commander {
+    background: url(../icons/commander.png) 0 0 no-repeat; // 46px
+    background-position: center;
+  }
+
+  // polaris
+  &akagi_mu { background: url(../icons/polaris.png) -80px 0; }
+  &hipper_mu { background: url(../icons/polaris.png) -40px 0; }
+
+
+}
+</style>
